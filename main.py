c03da75f76b45add9153ff8f930b07528f576f22
Angel España <a.espaben@gmail.com>
2020-03-14 18:53:02 -0430

    Balas, y detección de colisiones.

--- main.py
+++ main.py
@@ -1,83 +1,111 @@
 import pygame
-
-# Initialize the pygame
+import math
+from player import player
+# Initialize the pygame.
 pygame.init()
 
-# Title and icon
+# Title and icon.
 pygame.display.set_caption("Space invaders")
 icon = pygame.image.load("src/ufo.png")
 
 pygame.display.set_icon(icon)
-# Create the screen
+# Create the screen.
 screen = pygame.display.set_mode((800, 600))
 
+# Background.
+bg = pygame.image.load("src/background.jpg")
 
-# Player
+# Player.
 player_img = pygame.image.load("src/ship.png")
-player_x = 370
-player_y = 480
-player_speed = 0.8
-player_x_change = 0
+player1 = player(speed = 5, img = player_img)
 
-# Enemy
-enemy_img = pygame.image.load("src/ufo.png")
+# Bullet.
+bullet_img = pygame.image.load("src/bullet.png")
+bullet_x = 0
+bullet_y = 0
+bullet_ready = True
+
+# Enemy.
+enemy_img = pygame.image.load("src/enemy.png")
 enemy_x = 10
 enemy_y = 50
-enemy_speed = 0.2
+enemy_speed = 3
 enemy_change = 0
 
-def player(x, y):
-  screen.blit(player_img, (x, y))
-
+# Enemy drawing.
 def enemy(x, y):
   screen.blit(enemy_img, (x, y))
 
+# Bullet drawing.
+def shoot(x, y):
+  global bullet_ready
+  bullet_ready = False 
+  screen.blit(bullet_img, (x + 20, y - 10))
 
-# Game loop
+# Check if it's a collision.
+def is_collision(enemy_x, enemy_y, bullet_x, bullet_y):
+  distance = math.sqrt(math.pow(enemy_x - bullet_x, 2) + math.pow(enemy_y - bullet_y, 2))
+  return distance <= 60
+
+
+# Game loop.
 running = True
 
 while running:
-  # Change background color - RGB
-  screen.fill((0, 0, 0))
+  # Change background color - RGB.
+  keys = pygame.key.get_pressed()
+  screen.blit(bg, (0, 0))
 
+  # Check if exit button has been pressed.
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
 
-    # If keystroke is pressed, check whether it's left or right
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_LEFT:
-        player_x_change = -player_speed
+  if keys[pygame.K_LEFT]:
+    player1.move_left()
 
-      if event.key == pygame.K_RIGHT:
-        player_x_change = player_speed
-    
-    if event.type == pygame.KEYUP:
-      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-        player_x_change = 0
+  if keys[pygame.K_RIGHT]:
+    player1.move_right()
 
-  # Draw player
-  player_x += player_x_change
-  
-  if player_x < 10: player_x = 10
-  if player_x > 728: player_x = 728
+  # Collision detection.
+  collision = is_collision(enemy_x, enemy_y, bullet_x, bullet_y)
+  if collision:
+    bullet_y = player1.get_y()
+    bullet_ready = True
+    enemy_x = 10
+    enemy_y = 10
 
-  player(player_x, player_y)
-
-  # Drar enemy
-
+  # Draw enemy.
   enemy_x += enemy_change
 
-  if enemy_x >= 758:
+  if enemy_x >= 728:
     enemy_change = -enemy_speed
     enemy_y += 15
-    if enemy_speed < 0.8: enemy_speed += 0.02
+    if enemy_speed < 0.8: enemy_speed += 0.05
   elif enemy_x <= 10:
     enemy_change = enemy_speed
     enemy_y += 15
-    if enemy_speed < 0.8: enemy_speed += 0.02
+    if enemy_speed < 0.8: enemy_speed += 0.05
 
   enemy(enemy_x, enemy_y)
 
-  # Update changes
+  # Draw player.
+  player1.draw(screen)
+
+  # Draw bullet.
+  if bullet_ready and keys[pygame.K_SPACE]:
+    bullet_x = player1.get_x()
+    bullet_y = player1.get_y()
+    shoot(bullet_x, bullet_y)
+    bullet_ready = False  
+
+  if bullet_y <= 0:
+    bullet_y = player1.get_y()
+    bullet_ready = True
+  elif bullet_ready == False:
+    shoot(bullet_x, bullet_y)
+    bullet_y -= 5
+    
+
+  # Update screen.
   pygame.display.update()
--- player.py
+++ player.py
@@ -1,17 +1,26 @@
 class player:
-  def __init__(x = 370, y = 480, speed = 0.8, img = ""):
+  def __init__(self, x = 370, y = 480, speed = 0.8, img = None):
     self.x = x
     self.y = y
     self.speed = speed
     self.img = img
 
-  def draw(screen):
-    screen.blit(self.img, self.x, self.y)
+  def draw(self, screen):
+    screen.blit(self.img, (self.x, self.y))
 
-  def move_left():
+  def move_left(self):
     if self.x > 10:
-      x -= self.speed
+      self.x -= self.speed
   
-  def move_right():
+  def move_right(self):
     if self.x < 728:
-      x += self.speed
+      self.x += self.speed
+
+  def get_x(self):
+    return self.x
+  
+  def get_y(self):
+    return self.y
+
+  def get_speed(self):
+    return self.speed
